fe7713dc487d07469d2fe47a148e40d492a3baed
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-05-21 15:15:38 -0300

    Cria adapter e ViewHolder, lista exibição de postagem, ajustes de layout, adiciona recursos de imagens e strings

---
+++ app/src/main/java/br/com/estudo/facebookfeedclone/model/Post.java
@@ -0,0 +1,68 @@
+package br.com.estudo.facebookfeedclone.model;
+
+public class Post {
+
+    int imageViewUser;
+    int imageViewPost;
+    String textViewTime;
+    String textViewUsername;
+    String textViewContent;
+    String textViewTitle;
+    String textViewSubtitle;
+
+    public int getImageViewUser() {
+        return imageViewUser;
+    }
+
+    public void setImageViewUser(int imageViewUser) {
+        this.imageViewUser = imageViewUser;
+    }
+
+    public int getImageViewPost() {
+        return imageViewPost;
+    }
+
+    public void setImageViewPost(int imageViewPost) {
+        this.imageViewPost = imageViewPost;
+    }
+
+    public String getTextViewTime() {
+        return textViewTime;
+    }
+
+    public void setTextViewTime(String textViewTime) {
+        this.textViewTime = textViewTime;
+    }
+
+    public String getTextViewUsername() {
+        return textViewUsername;
+    }
+
+    public void setTextViewUsername(String textViewUsername) {
+        this.textViewUsername = textViewUsername;
+    }
+
+    public String getTextViewContent() {
+        return textViewContent;
+    }
+
+    public void setTextViewContent(String textViewContent) {
+        this.textViewContent = textViewContent;
+    }
+
+    public String getTextViewTitle() {
+        return textViewTitle;
+    }
+
+    public void setTextViewTitle(String textViewTitle) {
+        this.textViewTitle = textViewTitle;
+    }
+
+    public String getTextViewSubtitle() {
+        return textViewSubtitle;
+    }
+
+    public void setTextViewSubtitle(String textViewSubtitle) {
+        this.textViewSubtitle = textViewSubtitle;
+    }
+}
